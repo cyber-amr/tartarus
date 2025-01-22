46843448cede5fc28fec55377f59cac512900c83
Amr <amr@programmer.net>
2025-01-22 17:52:32 +0300

    initialize express.js project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const { join } = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(join(__dirname, "public"), { 
+	maxAge: 600000 // 10 min
+}))
+
+app.listen(PORT, () => {
+	console.log(`Listening at http://localhost:${PORT}`)
+})
