3db1d0cf8d4234cd523de7fb90cde963a60c47ba
Amr <amr@programmer.net>
2025-01-28 21:25:04 +0300

    reformat routes.js

--- routes.js
+++ routes.js
@@ -3,9 +3,7 @@ const path = require('path')
 const db = require("./db.js")
 
 // Static page routes
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'html', 'index.html'))
-})
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html', 'index.html')))
 
 // API routes
 router.post('/api/available-username', async (req, res) => {
@@ -43,8 +41,6 @@ router.use((err, req, res, next) => {
 })
 
 // 404 handler
-router.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'html', '404.html'))
-})
+router.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'html', '404.html')))
 
 module.exports = router;
