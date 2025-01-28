efcd6832524411623f59e7ffef584c0643cd834c
Amr <amr@programmer.net>
2025-01-28 21:21:49 +0300

    add registered-email api request

--- routes.js
+++ routes.js
@@ -22,6 +22,20 @@ router.post('/api/available-username', async (req, res) => {
     res.json({ available: !(await db.collection("users").findOne({ username })) })
 })
 
+router.post('/api/registered-email', async (req, res) => {
+    const email = req.body?.email
+    if (!email) return res.status(400).json({ error: 'email is required' })
+
+    if (!db.ready) return res.status(503).json({
+        error: 'Service Unavailable',
+        message: 'We experiencing high load. Please try again later.',
+    })
+
+    // TODO: add rateLimits
+
+    res.json({ registered: !!(await db.collection("users").findOne({ email })) })
+})
+
 // Error handling middleware
 router.use((err, req, res, next) => {
     console.error(err.stack);
