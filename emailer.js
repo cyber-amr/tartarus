1cc44b4c14996e110b54bf32886969b990a1690c
Amr <amr@programmer.net>
2025-02-17 15:56:23 +0300

    create advanced verification system

--- emailer.js
+++ emailer.js
@@ -1,8 +1,39 @@
+const SIGNABLE_PROVIDERS = []
+const VERIFICATION_SPAN = 30 * 60 * 1000 // (30min) in ms
+const VERIFICATION_REFRESH_AFTER = 1 * 60 * 1000 // (5min) in ms
+const VERIFICATION_EMAIL_BODY = `Welcome to Tartarus,
+
+Someone (hopefully you) requested to [reason] using this email address.
+
+Verify using the following code:
+
+
+[verification_code]
+
+
+Valid for 30 minutes.
+Never share this code with anyone
+If you did not request this, please ignore.
+Your email address won't be used unless verified.
+
+
+Need help? Contact us at contact@tartarus.space
+
+---
+Kind regards,
+The Tartarus System
+
+---
+This is an automated message.
+Please do not reply to this email.`
+
 const { SESv2Client, SendEmailCommand } = require("@aws-sdk/client-sesv2")
+const { randomUUID } = require("crypto")
 const { readFileSync } = require('fs')
 const forge = require('node-forge')
 const quotedPrintable = require('quoted-printable')
 const utf8 = require('utf8')
+const db = require("./db")
 
 const client = new SESv2Client({
     region: process.env.AWS_REGION ?? 'me-south-1', // Bahrain
@@ -92,4 +123,50 @@ if (process.env.SMIME_FULLCHAIN_PEM_PATH) {
             Content: { Raw: { Data: Buffer.from(message) } }
         }))
     }
+}
+
+module.exports.createVerification = async (email, reason) => {
+    const token = randomUUID().slice(0, 8).toUpperCase()
+    const expireDate = new Date(Date.now() + VERIFICATION_SPAN)
+
+    try {
+        const result = await db.collection('verifications').insertOne({ email, token, expireDate, reason })
+        if (!result.acknowledged) throw new Error()
+    } catch {
+        return { errorCode: 500, error: 'MongoDB error, could not insert new verification data' }
+    }
+    return { token, expireDate }
+}
+
+module.exports.sendVerificationEmail = async (email, reason) => {
+    let stored = await db.collection('verifications').findOne({ email, reason })
+    if (stored && new Date(stored.expireDate).getTime() - VERIFICATION_SPAN + VERIFICATION_REFRESH_AFTER < Date.now()) {
+        await db.collection('verifications').deleteOne(stored)
+        stored = undefined
+    }
+    const { token } = stored ?? await this.createVerification(email, reason)
+
+    const send = SIGNABLE_PROVIDERS.includes(email.split('@')[1]) ? this.sendSignedEmail : this.sendEmail
+    try {
+        const body = VERIFICATION_EMAIL_BODY
+            .replace('[reason]', reason)
+            .replace('[verification_code]', token.slice(0, 4) + ' ' + token.slice(4))
+
+        await send({ to: email, subject: "Email Verification", body })
+    } catch (error) {
+        return {
+            errorCode: error.$metadata?.httpStatusCode ?? 500,
+            error: 'Failed to send verification email'
+        }
+    }
+    return { sent: true }
+}
+
+module.exports.destroyVerification = (email, { reason, token }) => {
+    db.collection('verifications').deleteOne({ email, reason, token })
+}
+
+module.exports.isValidVerification = async (email, token) => {
+    const data = await db.collection('verifications').findOne({ email, token })
+    return data && new Date(data.expireDate).getTime() > Date.now()
 }
