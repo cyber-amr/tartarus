74c2713f5853607dac59fe5313aae45bd05dc4f4
Amr <amr@programmer.net>
2025-02-23 20:14:50 +0300

    merge verification routes into auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const path = require('path')
 const { sessionParser } = require('../sessioner')
 const { rateLimit } = require('express-rate-limit')
 const { isStr, isEmail, isToken, isUsername, isPassword, isDateIn, isDisplayName } = require('../validater')
-const { isValidVerification, destroyVerification } = require('../emailer')
+const { sendVerificationEmail, isValidVerification, destroyVerification } = require('../emailer')
 const { createUser, login } = require('../userer')
 
 const getIP = (req) => req.headers['x-forwarded-for'] ?? req.ip
@@ -78,6 +78,41 @@ router.get('/logout', sessionParser({ required: true }), (req, res) => res.sendF
 router.post('/logout', sessionParser({ required: true }), async (req, res) => {
     req.session.destroy(res)
     res.status(200).redirect('/')
+})
+
+router.post('/request-verification/', sessionParser(), rateLimit({
+    keyGenerator: (req) => req.body?.email ?? getIP(req),
+    skipFailedRequests: true,
+    limit: 3,
+    windowMs: 30 * 60 * 1000
+}), async (req, res) => {
+    if (req.session) return res.status(403).json({ error: 'Forbidden' })
+
+    const email = req.body?.email
+    if (!email) return res.status(400).json({ error: 'email is required' })
+    if (!isEmail(email)) return res.status(400).json({ error: 'Unsupported email format' })
+
+    const { error, errorCode, sent } = await sendVerificationEmail(email, 'create an account')
+
+    if (error) return res.status(errorCode ?? 400).json({ error })
+
+    res.status(202).json({ sent })
+})
+
+router.post('/is-valid-verification', sessionParser(), rateLimit({
+    keyGenerator: (req) => req.body?.email ?? getIP(req),
+    skipSuccessfulRequests: true,
+    limit: 3,
+    windowMs: 30 * 60 * 1000,
+}), (req, res) => {
+    if (req.session) return res.status(403).json({ error: 'Forbidden' })
+
+    const email = req.body?.email
+    const token = req.body?.token
+
+    if (!isEmail(email) || !isToken(token)) return res.status(400).json({ error: 'email and token are required' })
+
+    isValidVerification(email, token).then(isValid => res.status(200).json({ isValid }))
 })
 
 module.exports = router;
