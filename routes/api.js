43ae9101da09b131d79a06ef4cbfab647e9d4c66
Amr <amr@programmer.net>
2025-02-23 17:29:02 +0300

    claer

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const { rateLimit } = require('express-rate-limit')
 const { isUsername, isEmail } = require('../validater')
+const { User, SecretUser } = require('../userer')
 
 const getIP = (req) => req.headers['x-forwarded-for'] ?? req.ip
 
 router.use('/api', rateLimit({
-    keyGenerator: (req) => getIP(req),
+    keyGenerator: getIP,
     limit: 15,
     windowMs: 30 * 1000
 }))
@@ -14,7 +15,7 @@ router.post('/api/available-username', async (req, res) => {
     const username = req.body?.username
     if (!isUsername(username)) return res.status(400).json({ error: 'username is required, max 16 characters of A-z, 0-9 and _ only' })
 
-    res.json({ available: !(await db.collection("users").findOne({ username })) })
+    res.json({ available: !User.exists({ username }) })
 })
 
 router.post('/api/registered-email', async (req, res) => {
@@ -22,7 +23,7 @@ router.post('/api/registered-email', async (req, res) => {
     if (!email) return res.status(400).json({ error: 'email is required' })
     if (!isEmail(email)) return res.status(400).json({ error: 'Unsupported email format' })
 
-    res.json({ registered: !!(await db.collection("secrets").findOne({ email })) })
+    res.json({ registered: SecretUser.exists({ email }) })
 })
 
 module.exports = router;
